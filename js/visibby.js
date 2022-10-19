e7c577cd0aa7aa637cb739b7e6a7c1bec3676a15
Caramel-Tatto <tatto@caramel-code.com>
2022-10-19 13:11:37 +0900

    add scrolling to top function only js file yet

--- js/visibby.js
+++ js/visibby.js
@@ -58,9 +58,23 @@ _window.trigger('scroll');
 function scrollend(){
 	let elm = document.documentElement;
 
-	// scrollHeight ページの高さ clientHeight ブラウザの高さ
+	// scrollHeight ページの高さ clientHeight ブラウザの高さ　(bottom=一番下までスクロールしたときのtopの値)
 	let bottom = elm.scrollHeight - elm.clientHeight;
 
 	// 垂直方向へ移動
 	window.scroll(0, bottom);
 }
+
+//最下部に到達したときにスクロールボタンを変更
+const allHeight = Math.max(
+  document.body.scrollHeight, document.documentElement.scrollHeight,
+  document.body.offsetHeight, document.documentElement.offsetHeight,
+  document.body.clientHeight, document.documentElement.clientHeight
+);
+const mostBottom = allHeight - window.innerHeight;
+window.addEventListener('scroll', ()=> {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop >= mostBottom) {
+        // 最下部に到達したときに実行する処理
+    }
+});
